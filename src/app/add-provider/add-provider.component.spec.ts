467a64f2997bd0fadcc568703f52468394631436
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-02-26 19:12:37 +0000

    create table

--- src/app/add-provider/add-provider.component.spec.ts
+++ src/app/add-provider/add-provider.component.spec.ts
@@ -14,4 +14,5 @@ describe('AddProviderComponent', () => {
   });
 
   beforeEach(() => {
-    fixture = TestBed.createCompone
+    fixture = TestBed.createComponent(AddProviderComponent);
+    component = fixture.componentInstance;
