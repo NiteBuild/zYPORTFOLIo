d0350d60cafa238ceffe3979cb76e617d025b397
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-03-26 21:07:36 +0000

    Force push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,4 +2,5 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddItemComponent } from './add-item/add-item.component';
 import { InOutComponent } from './in-out/in-out.component';
-import { ItemDetailsComponent } from './it
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ProductListComponent } from './produc
