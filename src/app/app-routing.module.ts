38d478f7dc097dd7d12cb8cf407825667c126073
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-03-26 13:42:41 +0000

    msm: mdss: fix race condition between shutdown thread and fb ioctl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddItemComponent } from './add-item/add-item.component';
-import { InOutC
+import { InOutComponent } from './in-out/in-out.component';
+import { ItemDetailsComponent } from './it
