1303325fbe0dbc123021e2e23e0030cd407199aa
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-03-27 09:06:24 +0000

    Resolved merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,4 +4,7 @@ import { AddItemComponent } from './add-item/add-item.component';
 import { InOutComponent } from './in-out/in-out.component';
 import { ItemDetailsComponent } from './item-details/item-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProvidersListComponent } from './providers-list/prov
+import { ProvidersListComponent } from './providers-list/providers-list.component';
+
+const routes: Routes = [
+    { path: "item
