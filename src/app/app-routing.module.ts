7ad736e048cd78bdb61a09a86a7d34ee3759bf39
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-04-03 22:39:08 +0000

    Fixed the documentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,4 +16,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-    imports: [RouterModule.forRoot(route
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
