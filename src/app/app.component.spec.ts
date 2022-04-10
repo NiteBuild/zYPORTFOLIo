2262adb3b75cb57c3173395aa4d829863af7cc5d
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-04-10 06:51:43 +0000

    Update cart.php

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,4 +16,8 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const a
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  
