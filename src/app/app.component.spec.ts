b5bd3f134b2156b5fd7074bf260167b30901634c
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-04-17 20:10:29 +0000

    hrtimer: Prevent remote enqueue of leftmost timers

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,4 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent)
+    expect(compiled.querySelector('.content span')?.textContent).toContain('stock app is running!');
+  });
+});
