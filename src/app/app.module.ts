4a965ebfdb4ed295a2f9edbe95b05729e76b04af
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-04-23 21:10:59 +0000

    more icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,4 +2,5 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } f
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-rou
