f62f1850a7d4c75751a400732d4c7fc80f0b1b74
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-05-04 10:48:18 +0000

    Removed unused function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,4 +12,5 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AddItemComponent } from './add-item/add-item.component';
 import { InOutComponent } from './in-out/in-out.component';
-import { ProvidersListComponent } from './providers-list/providers-list.comp
+import { ProvidersListComponent } from './providers-list/providers-list.component';
+import { AddProviderComponent } from './add-provider/add-prov
