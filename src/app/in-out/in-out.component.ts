50ac486bff5b0875bd07378d49021087e786d7b6
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-06-18 13:59:16 +0000

    task 4123

--- src/app/in-out/in-out.component.ts
+++ src/app/in-out/in-out.component.ts
@@ -9,3 +9,59 @@ import { QuaggaJSResultObject } from '@ericblade/quagga2';
 @Component({
     selector: 'app-in-out',
     templateUrl: './in-out.component.html',
+    styleUrls: ['./in-out.component.scss']
+})
+export class InOutComponent implements AfterViewInit {
+    barcode: string;
+    product: Product | undefined;
+    form: FormGroup;
+    @ViewChild(BarcodeScannerLivestreamComponent)
+    barcodeScanner: BarcodeScannerLivestreamComponent;
+   
+    constructor(private productService: ProductService) {
+        this.form = new FormGroup({
+            units: new FormControl()
+        });
+    }
+
+    ngAfterViewInit(): void {
+        this.barcodeScanner.start();
+    }
+    onChange() {
+        if (this.barcode !== "") {
+            this.productService.getByBarcode(this.barcode).subscribe(
+                (prod) => {
+                    if (prod) {
+                        this.product = prod;
+                        this.form.controls.units.setValidators([Validators.min((prod.units ?? 0) * -1)]);
+                    }
+                    else
+                        this.product = undefined;
+                }
+            )
+        }
+        else
+            this.product = undefined;
+
+    }
+
+    onSubmit() {
+        if (this.product?.barcode) {
+            this.productService.changeQuantity(this.product.barcode, this.form.controls.units.value);
+            this.product.units += this.form.value.units;
+            this.updateValidator();
+        }
+    }
+
+    updateValidator() {
+        if (this.product?.units)
+            this.form.controls.units.setValidators([Validators.min(this.product.units * -1)]);
+    }
+
+
+    onValueChangesScanner(result: QuaggaJSResultObject) {
+        this.barcode = result.codeResult.code ?? "";
+        this.onChange();
+    }
+
+}
