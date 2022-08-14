2d214ef2628beb10b264f65c1391b04296fcc428
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-08-14 10:15:30 +0000

    Staging: android: binder: More offset validation.

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -2,4 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Product } from '../classes/product';
 import { ProductService } from '../services/product.service';
-import { Stat } from '../class
+import { Stat } from '../classes/stat';
+
+@Component({
+    selector: 'app-item-details',
+    templateUrl: './ite
