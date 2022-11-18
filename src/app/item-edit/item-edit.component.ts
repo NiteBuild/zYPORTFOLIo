acfaedcb984b25b2d1c31b7e40ad70021ee6882a
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-11-18 18:45:22 +0000

    Fix YAML

--- src/app/item-edit/item-edit.component.ts
+++ src/app/item-edit/item-edit.component.ts
@@ -3,4 +3,5 @@ import { FormControl, FormGroup } from '@angular/forms';
 
 import { Category, Product } from '../classes/product';
 import { Provider } from '../classes/provider';
-import { ProductService } from '../services/pr
+import { ProductService } from '../services/product.service';
+import { ProviderService } from '../servi
