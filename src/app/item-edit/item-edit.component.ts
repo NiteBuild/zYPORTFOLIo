0da309b6b2f8f2ac954b8b1f1ff96f355e758ae4
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2022-12-11 16:51:09 +0000

    remove tests

--- src/app/item-edit/item-edit.component.ts
+++ src/app/item-edit/item-edit.component.ts
@@ -42,4 +42,5 @@ export class ItemEditComponent implements OnInit {
     onSubmit() {
         this.product.model = this.form.value.model;
         this.product.brand = this.form.value.brand;
-        this.product.
+        this.product.barcode = this.form.value.barcode;
+        this.product.description = this.
