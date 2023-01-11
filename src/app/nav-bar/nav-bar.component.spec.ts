c2ee819517610989a42def6f31cb814136ff5389
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-01-11 19:25:57 +0000

    gpio

--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,3 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavBarComponent
+import { NavBarComponent } from './nav-bar.component';
+
+describe('NavBarComponent', () => {
+  let compone
