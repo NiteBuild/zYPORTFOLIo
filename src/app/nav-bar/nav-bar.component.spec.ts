f8e054f2617174f3550253baa458fdb69adfbab0
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-01-14 17:48:57 +0000

    Update a comment

--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -3,4 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavBarComponent } from './nav-bar.component';
 
 describe('NavBarComponent', () => {
-  let compone
+  let component: NavBarComponent;
+  let fixture: ComponentFixture<NavBarComponent>;
+
+  beforeEach(as
