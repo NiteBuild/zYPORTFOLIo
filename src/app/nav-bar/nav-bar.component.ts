f2775c46abb27f9bf5c4ad005dc3a9b83f446905
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-02-06 11:22:56 +0000

    Forward port zendframework/zf2#3887

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -3,4 +3,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
-  templateUrl: './nav
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class
