d07ad541df546164dc743630caf8e98e96897a4e
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-06 09:06:14 +0000

    Add changelog item

--- src/app/product-list/product-list.component.spec.ts
+++ src/app/product-list/product-list.component.spec.ts
@@ -1,3 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductListComponent } from './product-list.compone
+import { ProductListComponent } from './product-list.component';
+
+describe('ProductListComponent', () => {
+  let component: ProductListCom
