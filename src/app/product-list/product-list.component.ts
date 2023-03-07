29bf1b966115337f1110ba7719c1e5e27d03977f
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-07 20:07:52 +0000

    Bump version to 1.4.2

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Product } from '../classes/product';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from "../services/product.service";
-import { MatPaginator } from
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/ma
