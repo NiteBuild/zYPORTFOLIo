fdc71f030e6c4a907426e39d9973923e1b0fb6fa
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-08 09:50:56 +0000

    Update rt_waterfall_D3S.py

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,4 +4,9 @@ import { ProductService } from "../services/product.service";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
-import { Rout
+import { Router } from '@angular/router';
+
+
+@Component({
+    selector: 'app-product-list',
+    templateUr
