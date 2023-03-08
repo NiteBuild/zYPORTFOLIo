48b6459766734f9e1a85e7f7964354377ae90ce7
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-08 05:34:59 +0000

    Updated README documentation

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,4 +2,6 @@ import { Product } from '../classes/product';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from "../services/product.service";
 import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/ma
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { Rout
