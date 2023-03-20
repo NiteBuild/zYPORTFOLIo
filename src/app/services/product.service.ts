bc8adea28c63d3bdfae34ef03b34117e5c02c53c
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-20 20:48:51 +0000

    Modified license

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,3 +8,32 @@ import { Category, Product } from './../classes/product';
 @Injectable({
     providedIn: 'root'
 })
+export class ProductService {
+    private rootUrl = "http://localhost:8080/";
+
+    constructor(private http: HttpClient) { }
+
+    getAll(): Observable<Product[]> {
+        return this.http.get<Product[]>(this.rootUrl + "products");
+    }
+
+    getByBarcode(barcode: string) {
+        return this.http.get<Product>(this.rootUrl + "products/barcode/" + barcode);
+    }
+
+    editProduct(prod: Product) {
+        return this.http.put(this.rootUrl+"products", prod).subscribe(res=>console.log(res));
+    }
+
+    addProduct(prod: Product) {
+        return this.http.post(this.rootUrl + "products", prod);
+    }
+
+    changeQuantity(barcode: string, value: any) {
+        return this.http.post(this.rootUrl + "movements", { barcode, quantity: value }).subscribe(res=>console.log(res));
+    }
+
+    getStats(productId:number){
+        return this.http.get<Stat>(this.rootUrl+"products/"+productId+"/stats");
+    }
+}
---
+++ src/app/services/provider.service.spec.ts
@@ -0,0 +1,1 @@
+import { TestBed } from '@ang
