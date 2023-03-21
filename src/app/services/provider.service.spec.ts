cba193167c001c9da8694be689aca475619ad8e9
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-21 06:52:07 +0000

    Update documentation/WindRiver.md

--- src/app/services/provider.service.spec.ts
+++ src/app/services/provider.service.spec.ts
@@ -2,4 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ProviderService } from './provider.service';
 
-describe('
+describe('ProviderService', () => {
+  let service: ProviderService;
+
+  beforeEach(() 
