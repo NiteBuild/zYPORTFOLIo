9f842d8b9fd0c4836135c149f88bf09e03c645ee
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-24 22:17:01 +0000

    futexes: Increase hash table size for better performance

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environ
