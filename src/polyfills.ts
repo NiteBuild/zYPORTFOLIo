e1b175687d6cbdc68f25781264aa82938471d0f6
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-30 17:13:44 +0000

    Clean-up code

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -32,4 +32,4 @@ import '@angular/localize/init';
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
-// imp
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
