9bffd044717842d29eb38cac02d66682aa7e86f5
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-03-31 20:41:12 +0000

    langpacks/russian: update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -38,4 +38,5 @@ import '@angular/localize/init';
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  * because those flags need to be set before `zone.js` being loaded, and webpack
- * will put import in the top of bundle, so user need to c
+ * will put import in the top of bundle, so user need to create a separate file
+ * in this directory (for example: zone-fl
