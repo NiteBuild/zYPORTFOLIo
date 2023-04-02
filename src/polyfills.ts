197e1a347129bbc961928a67649f62b78637ad36
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-04-02 08:48:01 +0000

    New translations strings.xml (Serbian (Latin))

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -39,4 +39,5 @@ import '@angular/localize/init';
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  * because those flags need to be set before `zone.js` being loaded, and webpack
  * will put import in the top of bundle, so user need to create a separate file
- * in this directory (for example: zone-fl
+ * in this directory (for example: zone-flags.ts), and put the following flags
+ * into that file, and t
