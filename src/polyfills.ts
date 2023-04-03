19fae597935184910286df6649200a93cbb6dc3f
NiteBuild <110361614+NiteBuild@users.noreply.github.com>
2023-04-03 08:37:32 +0000

    fix panic

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -48,4 +48,5 @@ import '@angular/localize/init';
  * The following flags will work for all browsers.
  *
  * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- * (window as any).__Zone_disable_on_property = true; // disable
+ * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+ * (window as any).__zone_symbol__UNPATCHED_EVENTS =
